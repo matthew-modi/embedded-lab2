048291518eb1d3156c6b0248f5c7c3484f4f2d07
Matthew Modi <22771068+matthew-modi@users.noreply.github.com>
2025-03-05 23:23:08 -0500

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -187,7 +187,7 @@ int main(){
                                 fbputchar(message[i], fbmaxrows() - 1 - INPUT_ROWS + (i / fbmaxcols()), i % fbmaxcols());
                             }
                             printf("message_len: %d\n", message_len);
-                            message[message_len-1] = ' ';
+                            // message[message_len-1] = ' ';
                             printf("message[message_len-1]: %c\n", message[message_len-1]);
                             fbputchar(' ', fbmaxrows() - 1 - INPUT_ROWS + (message_len-1 / fbmaxcols()), message_len-1 % fbmaxcols());
                             message_len--;
