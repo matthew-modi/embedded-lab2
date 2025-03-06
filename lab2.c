ec6ebfce71481879c877028527bfd64da3046fd4
Matthew Modi <22771068+matthew-modi@users.noreply.github.com>
2025-03-05 20:01:30 -0500

    minor fix

--- lab2.c
+++ lab2.c
@@ -142,7 +142,7 @@ void *network_thread_f(void *ignored){
     while ( (n = read(sockfd, &recvBuf, BUFFER_SIZE - 1)) > 0 ) {
         recvBuf[n] = '\0';
         printf("%s", recvBuf);
-        int len = strlen(msg);
+        int len = strlen(recvBuf);
         int pos = 0;
         char line[256];
 
